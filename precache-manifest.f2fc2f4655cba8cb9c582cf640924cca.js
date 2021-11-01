8e2eb3e8467deef15dac232c1e1c8e238b4aae65
ntnghia0320 <ntnghia0320@users.noreply.github.com>
2021-11-01 07:59:19 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.f2fc2f4655cba8cb9c582cf640924cca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a622b3bd974f915c9d1a3f2ddf81531",
+    "url": "https://ntnghia0320.github.io/task-management-frontend\n/index.html"
+  },
+  {
+    "revision": "3fbb35667f3690bde9c2",
+    "url": "https://ntnghia0320.github.io/task-management-frontend\n/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "3afdb998ec5b879066a2",
+    "url": "https://ntnghia0320.github.io/task-management-frontend\n/static/js/2.074ed3e4.chunk.js"
+  },
+  {
+    "revision": "3fbb35667f3690bde9c2",
+    "url": "https://ntnghia0320.github.io/task-management-frontend\n/static/js/main.e8c7abf8.chunk.js"
+  },
+  {
+    "revision": "edb4b8edb9b193f899c1",
+    "url": "https://ntnghia0320.github.io/task-management-frontend\n/static/js/runtime~main.e3f78404.js"
+  }
+]);
